7f75c55e9e2a391f29188d534f4582731dcd79a8
NightFiyah <heinvermaak@gmail.com>
2013-09-28 17:59:39 +0200

    Started work on ticker

--- TalesEstateServer/src/java/GameTicker/Tick.java
+++ TalesEstateServer/src/java/GameTicker/Tick.java
@@ -7,6 +7,7 @@ package GameTicker;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
@@ -48,8 +49,69 @@ public class Tick {
 
             if (rs != null) {
                 while (rs.next()) {
-                    System.out.println("PROCESSING BUILDING LOG ENTRY "+rs.getString("BuildLogID") );
-                    System.out.println(rs.getString("BuildLogCharacterID"));
+                    System.out.println("PROCESSING BUILDING LOG ENTRY " + rs.getString("BuildLogID"));
+
+                    int PlotID = Integer.parseInt(rs.getString("BuildLogPlotID"));
+
+                    int BuildingID = Integer.parseInt(rs.getString("BuildLogBuildingID"));
+                    System.out.println("retrieving buildig details");
+                    ArrayList<String[]> retrieveBuildingDetailsById = retrieveBuildingDetailsById(BuildingID);
+                    System.out.println("done retreiving " + retrieveBuildingDetailsById.get(0));
+                    double income = Double.parseDouble(retrieveBuildingDetailsById.get(0)[6]);
+                    int hapiness = Integer.parseInt(retrieveBuildingDetailsById.get(0)[10]);
+
+                    //needs to be but dont see in database
+                    double defenceValue = 0;
+
+                    double newIncome = 0;
+                    int newHappiness = 0;
+                    double newDefenceValue = 0;
+
+
+                    //before update need to find all the current values
+                    Statement current = con.createStatement();
+                    System.out.println("RETIEVE PLOT DETAILS");
+                    ResultSet rsC = current.executeQuery("SELECT * FROM Plot WHERE PlotID=" + PlotID);
+
+                    double currentInc = 0;
+                    int currentHap = 0;
+                    double currentDef = 0;
+
+                    rs.next();
+
+
+                    System.out.println("PLOT FOUND GETTING VALUES");
+                    currentInc = Double.parseDouble(rsC.getString("PlotMonthlyIncome"));
+
+                    currentHap = Integer.parseInt(rsC.getString("PlotHappiness"));
+
+                    currentDef = Double.parseDouble(rsC.getString("PlotDefenseValue"));
+
+
+
+
+
+
+                    System.out.println("STARTING UPDATE");
+
+                    Statement stmtIncome = null;
+                    stmtIncome = con.createStatement();
+
+                    // k now update income
+                    newIncome = currentInc + income;
+                    System.out.println("INCOME");
+                    stmtIncome.execute("UPDATE Plot SET PlotMonthlyIncome=" + newIncome + " WHERE PlotID=" + PlotID);
+
+                    // hapiness
+                    newHappiness = currentHap + hapiness;
+                    System.out.println("HAPINESS");
+                    stmtIncome.execute("UPDATE Plot SET PlotHappiness=" + newHappiness + " WHERE PlotID=" + PlotID);
+
+                    //defence
+                    newDefenceValue = currentDef + defenceValue;
+                    System.out.println("DEFENCE");
+                    stmtIncome.execute("UPDATE Plot SET PlotDefenseValue=" + newDefenceValue + " WHERE PlotID=" + PlotID);
+
                 }
             }
 
@@ -57,6 +119,7 @@ public class Tick {
         } catch (Exception e) {
             System.out.println("ticker error 1");
             System.out.println(e.getMessage());
+
         }
 
 
@@ -75,5 +138,112 @@ public class Tick {
 
     public void UpdateIncome() {
         // this adds each propertys income to the property
+    }
+
+    public ArrayList<String[]> retrieveBuildingDetailsById(int id) {
+        ArrayList<String[]> values;
+        String[] line;
+        String answer = "";
+        int count;
+
+        //Test if id exists
+
+        //Retrieve details
+        try {
+            sql = "SELECT * FROM Building WHERE BuildingID = " + id;
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            values = new ArrayList();
+
+            while (rs.next()) {
+                line = new String[11];
+                line[0] = rs.getString("BuildingTypeID");
+                line[1] = rs.getString("BuildingTypeOfIndustry");
+                line[2] = rs.getString("BuildingAvailabilityID");
+                line[3] = rs.getString("BuildingPrerequisiteID");
+                line[4] = rs.getString("BuildingCost");
+                line[5] = rs.getString("BuildingSetupCost");
+                line[6] = rs.getString("BuildingMonthlyIncome");
+                line[7] = rs.getString("BuildingWorkersNeeded");
+                line[8] = rs.getString("BuildingTimeToBuild");
+                line[9] = rs.getString("BuildingSizeRequired");
+                line[10] = rs.getString("BuildingHappiness");
+                values.add(line);
+            }
+
+            for (int a = 0; a < values.size(); a++) {
+                sql = "SELECT BuildingTypeDescription FROM BuildingType "
+                        + "WHERE BuildingTypeID = " + values.get(a)[0];
+                stmt = con.createStatement();
+                rs = stmt.executeQuery(sql);
+                rs.next();
+                values.get(a)[0] = rs.getString("BuildingTypeDescription");
+
+                sql = "SELECT * FROM BuildingAvailability WHERE "
+                        + "BuildingAvailabilityID = " + values.get(a)[2];
+                stmt = con.createStatement();
+                rs = stmt.executeQuery(sql);
+                rs.next();
+                count = 0;
+                answer = "";
+
+                if (rs.getString("BuildingAvailabilityThegnheim").equals("1")) {
+                    answer = "Thegnheim";
+                    ++count;
+                }
+                if (rs.getString("BuildingAvailabilitySarkland").equals("1")) {
+                    if (count > 0) {
+                        answer += ",Sarkland";
+                    } else {
+                        answer += "Sarkland";
+                    }
+                }
+                if (rs.getString("BuildingAvailabilityRagonvaldr").equals("1")) {
+                    if (count > 0) {
+                        answer += ",Ragonvaldr";
+                    } else {
+                        answer += "Ragonvaldr";
+                    }
+                }
+                if (rs.getString("BuildingAvailabilitySvaerstein").equals("1")) {
+                    if (count > 0) {
+                        answer += ",Svaerstein";
+                    } else {
+                        answer += "Svaerstein";
+                    }
+                }
+                if (rs.getString("BuildingAvailabilityRotheim").equals("1")) {
+                    if (count > 0) {
+                        answer += ",Rotheim";
+                    } else {
+                        answer += "Rotheim";
+                    }
+                }
+                if (rs.getString("BuildingAvailabilityLangzerund").equals("1")) {
+                    if (count > 0) {
+                        answer += ",Langzerund";
+                    } else {
+                        answer += "Langzerund";
+                    }
+                }
+
+                values.get(a)[2] = answer;
+
+                sql = "SELECT BuildingPrerequisiteDescription FROM BuildingPrerequisite "
+                        + "WHERE BuildingPrerequisiteID = " + values.get(a)[3];
+                stmt = con.createStatement();
+                rs = stmt.executeQuery(sql);
+                rs.next();
+                values.get(a)[3] = rs.getString("BuildingPrerequisiteDescription");
+            }
+
+            return values;
+        } catch (Exception e) {
+            System.out.println("Unable to execute function retrieveBuildingDetailsById()");
+            System.out.println(e.getMessage());
+        }
+
+        return null;
     }
 }
